99830c68b55fac6e9d8eb6e6594e093b9db3ff23
Justinus Adriaanse <jadriaanse@me.com>
2014-03-11 11:10:59 -0600

    Working on the Wizzard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Refer::Application.routes.draw do
-  get "wizzard/step1"
-  get "wizzard/step2"
-  get "wizzard/step3"
-  get "wizzard/step4"
+  get "wizzard/step1", as: 'step1'
+  post "wizzard/step2", as: 'step2'
+  post "wizzard/step3", as: 'step3'
+  post "wizzard/step4", as: 'step4'
   get "sessions/new" => 'sessions#new'
   post "sessions/create", as: 'sessions'
   get "sessions/destroy"
