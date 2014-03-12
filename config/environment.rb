70375b52f2a022f1ad649ee2cbe44c112025c0eb
Justinus Adriaanse <jadriaanse@me.com>
2014-03-12 13:10:42 -0600

    Adding sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Refer::Application.initialize!
+
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+# Rails.application.routes.default_url_options[:host] = 'surerefer.herokuapp.com'
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'justinus',
+  :password => ENV['SGP'],
+  :domain => 'surerefer.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
