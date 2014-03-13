e03e6019ed391645cfd5e8613e62823251e2adb1
Justinus Adriaanse <jadriaanse@me.com>
2014-03-13 09:53:53 -0600

    Added lead to redirect link
    And setting variables for the agent and broker accept pages

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -3,22 +3,23 @@ class TokenController < ApplicationController
   def value
     token = Token.find_by(value: params[:value])
     if token.token_type == 'lead_agent'
-      redirect_to agent_accept_url
+      redirect_to agent_accept_url({lead_id: token.lead.id})
     elsif token.token_type == 'lead_broker'
-      redirect_to broker_accept_url
+      redirect_to broker_accept_url({lead_id: token.lead.id})
     elsif token.token_type == 'password_reset'
       if token.created_at > 7200
         render 'value'
       end
+      @user = token.user
       render 'new_password'
     end
   end
 
   def agent
-
+    @lead = Lead.find_by(id: params[:lead_id])
   end
 
   def broker
-
+    @lead = Lead.find_by(id: params[:lead_id])
   end
 end
