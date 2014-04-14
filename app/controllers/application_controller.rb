51eebc2abe4c8f7c448857808a38a22f3b3f97ec
Nathan Roise <nathanroise@gmail.com>
2014-04-14 17:14:53 -0600

    Updated password message & wizard/step1&2, styled sessions/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def auto_password_check
     if current_user.present? && current_user.auto_password
-      flash.now[:error] = "You still have the auto assigned password, <a href='#{set_password_url}'>Click Here</a> to change".html_safe
+      flash.now[:error] = "You have an auto-assigned password. <a href='#{set_password_url}'>Change it here</a> to make signing in easier.".html_safe
     end
   end
 
