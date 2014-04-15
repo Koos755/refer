f6eef9aff7bf3eb10c4e6f6962fa0404881679e2
Justinus Adriaanse <jadriaanse@me.com>
2014-04-14 23:32:47 -0600

    Added leads index page

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,14 +1,35 @@
 class LeadsController < ApplicationController
 
+  before_action :set_lead, only: [:show]
+  before_action :only_agents_and_broker_can_view_lead, only: [:show]
+
   def show
   end
 
   def index
+    if current_user.present?
+      if current_user.broker.present?
+        #TODO add association from broker to leads and then add variables here
+      else
+        @leads_send = current_user.agent.leads_send
+        @leads_received = current_user.agent.leads_received
+      end
+    else
+      flash[:error] = "You have to be logged in to view that page"
+      redirect_to root_url
+    end
   end
 
   private
 
   def set_lead
-    @lead = params[:id]
+    @lead = Lead.find_by(id: params[:id])
+  end
+
+  def only_agents_and_broker_can_view_lead
+    unless [@lead.sending_agent.user, @lead.receiving_agent.user, @lead.broker.user].include?(current_user)
+      flash[:error] = "You are not allowed to view this page!"
+      redirect_to root_url
+    end
   end
 end
