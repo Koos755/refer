c384f5f9af668bd5b18e0673c710ab35299dabd9
Justinus Adriaanse <jadriaanse@me.com>
2014-04-15 15:19:21 -0600

    Done Leads controller for index and show action

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,5 +1,6 @@
 class LeadsController < ApplicationController
 
+  before_action :only_current_users
   before_action :set_lead, only: [:show]
   before_action :only_agents_and_broker_can_view_lead, only: [:show]
 
@@ -7,16 +8,15 @@ class LeadsController < ApplicationController
   end
 
   def index
-    if current_user.present?
-      if current_user.broker.present?
-        #TODO add association from broker to leads and then add variables here
-      else
-        @leads_send = current_user.agent.leads_send
-        @leads_received = current_user.agent.leads_received
-      end
+    if current_user.broker.present?
+      @leads_send = current_user.broker.leads_send
+      @leads_received = current_user.broker.leads_received
+    elsif current_user.agent.present?
+      @leads_send = current_user.agent.leads_send
+      @leads_received = current_user.agent.leads_received
     else
-      flash[:error] = "You have to be logged in to view that page"
-      redirect_to root_url
+      @leads_send = []
+      @leads_received =[]
     end
   end
 
@@ -29,6 +29,13 @@ class LeadsController < ApplicationController
   def only_agents_and_broker_can_view_lead
     unless [@lead.sending_agent.user, @lead.receiving_agent.user, @lead.broker.user].include?(current_user)
       flash[:error] = "You are not allowed to view this page!"
+      redirect_to root_url
+    end
+  end
+
+  def only_current_users
+    unless current_user.present?
+      flash[:error] = "You have to be logged in to view that page"
       redirect_to root_url
     end
   end
