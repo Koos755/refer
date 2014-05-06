61c4e82acd4e4b0894b23f728343e54043b032f3
Justinus Adriaanse <jadriaanse@me.com>
2014-05-06 22:42:27 +0200

    PDF only works on agreement page

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Refer
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Central Time (US & Canada)'
-    config.middleware.use "PDFKit::Middleware", :print_media_type => true
+    config.middleware.use "PDFKit::Middleware", {}, :only => %r[/agreement]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
